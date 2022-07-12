d955dd4855c843748e4498500eeb54051a81b6cf
Grigory Sychev <100209515+grisha-sychev@users.noreply.github.com>
2022-07-12 19:08:02 +0300

    Create JsonSession.php

---
+++ JsonSession.php
@@ -0,0 +1,37 @@
+<?php
+
+class JsonSession
+{
+  private $path;
+  private $array;
+
+  public function file($file)
+  {
+    mkdir($_SERVER['DOCUMENT_ROOT'].'/src/JsonSession/', 0755);
+    $this->path = $_SERVER['DOCUMENT_ROOT'].'/src/JsonDB/'.$file.'.json';
+    $json = @file_get_contents($this->path);
+    $array = (array) json_decode($json, true);
+    return $this->array = $array;
+  }
+
+  public function set($key = [], $val = '')
+  {
+    $this->array[$key] = $val;
+    $json = json_encode($this->array, JSON_UNESCAPED_UNICODE);
+    file_put_contents($this->path, $json);
+  }
+
+  public function get($key = [])
+  {
+    return $this->array[$key];
+  }
+
+
+  public function del($key = [])
+  {
+    unset($this->array[$key]);
+    $json = json_encode($this->array, JSON_UNESCAPED_UNICODE);
+    file_put_contents($this->path, $json);
+  }
+
+}
